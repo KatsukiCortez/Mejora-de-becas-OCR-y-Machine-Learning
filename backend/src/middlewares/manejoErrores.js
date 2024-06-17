aa345604d2ec5c5a6571ee2e3bfdcb89fdf822d8
JacuKingu <71521139@continental.edu.pe>
2024-06-17 16:41:27 -0500

    Delete(cosas innecesarias)

--- backend/src/middlewares/manejoErrores.js
+++
@@ -1,9 +0,0 @@
-const { body, validationResult } = require('express-validator');
-
-const manejoErrores = (err, req, res, next) => {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  };
-  
-module.exports = manejoErrores;
-  
